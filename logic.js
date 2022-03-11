adba415353996737dc0aed29aa41a4b02863a711
Tikaram_Ac <tikaramac19@gmail.com>
2022-03-11 17:01:42 +0545

    modified

--- logic.js
+++ logic.js
@@ -10,3 +10,19 @@ $(document).ready(function () {
     $("header").removeClass("toggle");
   });
 });
+
+// validate form
+
+const validateForm = () => {
+  const fullname = document.forms["myform"]["name"].value;
+  const email = document.forms["myform"]["email"].value;
+  const textarea = document.forms["myform"]["textarea"].value;
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (fullname === "" || email === "") {
+    alert("you must need to fill all the field !");
+  } else {
+    if (textarea.length < 5) {
+      alert("message is to short !");
+    }
+  }
+};
